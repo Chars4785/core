4409837a51e33740f7d266ef320dfb1f53545a1a
Lee Jong Min <9105lgm@naver.com>
2023-02-24 01:08:01 +0900

    Ioc, DI 의존관계

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,17 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 구체적인건 이제 전혀 모르고
+    // 생성자를 통해서 들어간다 해서 "생성자 주입" 이라고 한다.
+    // 추상화에만 의존한다.
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {
